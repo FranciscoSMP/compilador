7bdbdb3f6259753aa1932c3d0c4d3dcb734f4d36
cristianh2003 <cristianh2003@gmail.com>
2025-05-24 15:57:11 -0600

    1.3
    Ejemplos 1, 2 y 3

---
+++ ply/ejemplo_1.cpp
@@ -0,0 +1,14 @@
+Cálculo de una expresión simple  
+/*  
+* programa de ejemplo No. 1  
+*/  
+int scanf( char *fmt, ... );  
+int printf( char *fmt, ... );  
+int main( void )  
+{  
+int i, j;  
+i = scanf("%d"); /* lea i */  
+j = 9 + i * 8; /* evalue j */  
+printf( "Resultado es %d\n", j ); /* imprima j */  
+}  
+Llamada simple a función  
---
+++ ply/ejemplo_3.cpp
@@ -0,0 +1,23 @@
+Una función recursiva simple  
+/*  
+* Calculo de factorial de un número  
+*/  
+int scanf( char *fmt, ... );  
+int printf( char *fmt, ... );  
+int factorial( int n );  
+int main( void )  
+{  
+int n, fact; /*declara las variables a y fact */  
+printf( "Ingrese un entero: " );  
+n = scanf( "%d"); /* lea n */  
+fact = factorial( n ); /* llame a factorial */  
+printf( "Factorial de %d ", n );  
+printf( "es %d\n", fact );  
+}  
+int factorial( int n )  
+{  
+if ( n <= 1 )  
+return 1;  
+else  
+return n * factorial( n - 1 );  
+}
